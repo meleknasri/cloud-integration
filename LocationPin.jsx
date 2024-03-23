b8a9a051baac006d319acba9c331102a61e5c048
meleknasri <150367418+meleknasri@users.noreply.github.com>
2024-03-23 01:17:27 +0100

    Add files via upload

---
+++ LocationPin.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LocationPin = ({ mawjoudin }) => (
+  <div className="pin">
+    <div className="pin-text">{text}</div>
+  </div>
+);
+
+export default LocationPin;
